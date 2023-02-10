0081f753a79c563e1df7d645a065ceab358bbbf9
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-02-10 00:13:01 -0800

    added new styling spec, API implementation not complete

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,15 @@ import Link from 'next/link';
 
 export default function Header(props) {
     return (
-        <header className=' flex items-center justify-between p-3 bg-emerald-500'>
-            <h1 className='text-3xl font-semibold'>Cookie Stand Admin</h1>
+        <header className=' flex content-evenly bg-emerald-500'>
+            <div className='w-3/4' >
+            <h1 className='text-3xl text-center font-semibold'>Cookie Stand Admin</h1>
+            </div>
+            <div className=' w-1/4'>
+            <Link href='/overview' className=' px-2 rounded-md bg-white hover:bg-blue-300'>"TD loggin user"</Link>
+            <Link href='/overview' className=' px-2 rounded-md bg-emerald-800 text-white hover:bg-blue-300'>Sign Out</Link>
             <Link href='/overview' className=' px-2 rounded-md bg-white hover:bg-blue-300'>Overview</Link>
+            </div>
             {/* <button className=' px-2 rounded-md bg-white hover:bg-blue-500'>Overview</button> */}
         </header>
     )
